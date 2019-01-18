c4484db55591f4f9eba9b136f4fc9374b23390ad
HARERIMANA FRANK <harfrank2@gmail.com>
2019-01-18 23:13:12 +0200

    allowing user to respond to a meetup

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,6 +6,8 @@ const routers = express.Router();
 routers.get('/meetups', meetupControllers.allMeetup);//cool
 routers.post('/meetups', meetupControllers.create);//cool
 routers.post('/meetups/:meetupId/rsvp', meetupControllers.rsvp);//cool
+routers.get('/meetups/:meetupId', meetupControllers.getsingleMeetup);
+routers.delete('/meetups/:meetupId', meetupControllers.deleteMeetup);
 
 
 module.exports = routers;
