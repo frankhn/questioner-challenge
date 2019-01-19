afa9d29538c431e2f4f60087bb1accae0d832e1d
HARERIMANA FRANK <harfrank2@gmail.com>
2019-01-19 06:41:48 +0200

    adding create comment API endpoint

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -9,9 +9,11 @@ routers.post('/meetups', meetupControllers.create);//cool
 routers.post('/meetups/:meetupId/rsvp', meetupControllers.rsvp);//cool
 
 
+routers.get('/meetups/:meetupId/questions', questionControllers.getQuestions);
+routers.post('/meetups/:meetupId/questions', questionControllers.create);//cool
 routers.patch('/questions/:questionId/downvote', questionControllers.downvote);
 routers.patch('/questions/:questionId/upvote', questionControllers.upvote);
-routers.get('/questions/:questionId/comments', questionControllers.getComments)
+routers.post('/questions/:questionId/comments', questionControllers.creatComment);
 routers.get('/meetups/:meetupId', meetupControllers.getsingleMeetup);
 routers.delete('/meetups/:meetupId', meetupControllers.deleteMeetup);
 
