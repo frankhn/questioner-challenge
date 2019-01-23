a5c2e3d6324d1747be3676653e0bced2bcc96f33
HARERIMANA FRANK <harfrank2@gmail.com>
2019-01-23 18:29:00 +0200

    connecting the database postgresql

---
+++ server/config/connection.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import {Pool} from "pg";
+
+const connection=new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
+});
+
+module.exports=connection;
