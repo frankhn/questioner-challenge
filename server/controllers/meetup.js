b0712c9ec180c5cd5380c0dd7e19bcc1aee6bf2d
HARERIMANA FRANK <harfrank2@gmail.com>
2019-01-23 19:00:44 +0200

    writting queries for meetup endpoints

--- server/controllers/meetup.js
+++ server/controllers/meetup.js
@@ -1,87 +1,22 @@
-import BaseJoi from 'joi';
-import Extension from 'joi-date-extensions';
-import meetups from '../models/meetup';
-import confirmMeetup from '../middleware/validate';
-import validater from '../middleware/validations'
 
-const Joi = BaseJoi.extend(Extension);
-
-class meetupController {
-  // get all meetups
-  allMeetup(req, res) {
-  res.status(200).json({
-    status: 200,
-    data: meetups,
-  });
-}// end of get all meetups
-
-
-
-create(req, res) {
-  const id = meetups.length + 1;
-  const today = new Date().toLocaleDateString();
-  Joi.validate(req.body, validater.meetupSchema, validater.validationOptions, (err, result) => {
-    if (!err) {
-      meetups.push(result);
-      res.status(201).json({
-        status: 201,
-        msg: 'you successfully created a Meetup',
-        object: result,
-      });
-    } else {
-      res.status(404).json({
-        status: 404,
-        msg: 'invalid inputs:please make sure you insert valid data',
-      });
-    }
-  });
-}// end of create a meetup
-
-
-// delete a meetup record
-deleteMeetup(req, res) {
-  const findmeetup = confirmMeetup(req.params.meetupId);
-  const meetupId = req.params.meetupId;
-  if (findmeetup) {
-    const filteredMeetups = meetups.filter(meetup => meetup !== meetups.indexOf);
-    res.status(202).json({
-      status: 202,
-      msg: 'resource accepted and marked for deletion',
-      data: filteredMeetups
-    });
-  } else { res.status(404).json({ status: 404, msg: 'meetup not found' }); }
-}// end of delete a meetup // object accepted for deletion 202
-
-
-// Respond to meetup RSVP
-rsvp(req, res) {
-	const findmeetup = confirmMeetup(req.params.meetupId);
-	if (findmeetup) {
-		res.status(200).json({
-    status: 200,
-    message: `thanks for responding to the meetup ${req.params.meetupId}`,
-  });
-} else {
-	res.status(404).json({
-		message: 'the meetup your trying to respond to does not exist'
-	});
+class Meetup{
+	constructor(location,topic,happening_on,image_name){
+		this.happening_on = happening_on;
+		this.location = location;
+		this.topic = topic;
+		this.image_name = image_name;
+	}
 }
-}//end of rsvp for a meetup
+module.exports = new Meetup();
 
-// Get a specific meetup record.
-getsingleMeetup(req, res) {
-  const findmeetup = confirmMeetup(req.params.meetupId);
-  if (findmeetup) {
-    res.status(200).json({ status: 200, data: findmeetup });
-  } else {
-    res.status(404).json({
-      status: 404,
-      data: `no such meetup found with ID ${req.params.meetupId}`,
-    });
-  }
-}// end of Get a specific meetup record.
+// const meetups = [
+// 	{
+// 		id: 1, createdOn: '12/12/2018', location: 'telecom house', images: 'upload/tel.jpg', topic: 'bootcamp', happeningOn: '19/12/2018', tags: '"questioner" "node js"',
+// 	},
+// 	{
+// 		id: 2, createdOn: '15/12/2018', location: 'telecom house', images: 'upload/tel2.jpg', topic: 'bootcamp cycle2', happeningOn: '25/12/2018', tags: '"questioner" "postgreSQL js"',
+// 	},
+// ];
 
 
-}
-const meetupControllers = new meetupController();
-module.exports = meetupControllers;
+// module.exports = meetups
