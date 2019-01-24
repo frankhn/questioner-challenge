3c3e695262391925ac3ba1035e1ea5063c71c958
HARERIMANA FRANK <harfrank2@gmail.com>
2019-01-24 11:17:19 +0200

    writing login authontication queries

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,8 +3,8 @@ import BaseJoi from 'joi';
 import Extension from 'joi-date-extensions';
 import validater from '../middleware/validations';
 import db from "../config/connection";
-import bcrypt  from 'bcryptjs'
-import jwt from  'jsonwebtoken'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
 const Joi = BaseJoi.extend(Extension);
 
 class meetupController {
@@ -16,7 +16,21 @@ class meetupController {
  */
 
 
- async login(req, res) {
+async login(req, res) {
+    const email =req.body.email;
+    const password =req.body.password;
+    db.query('SELECT email FROM user_table WHERE email = $1', [email])
+    .then((user) =>{
+      if(user.rows === undefined || user.rows.length == 0){
+        return res.status(404).json({msg: "invalid credentials"});
+      } else{
+       res.status(200).json({
+         msg: 'your done'
+       })
+}
+    })
+     
+
 }
 /**
  * 
@@ -24,14 +38,13 @@ class meetupController {
  * @param {*returns success if created} res 
  */
 async register(req, res) {
-    const hashedPassword = bcrypt.hashSync(req.body.password, 8);
+  const hashedPassword = bcrypt.hashSync(req.body.password, 8);
        db.query(`INSERT INTO user_table(firstname,lastname,othername,email,phone_number,username,password)
        VALUES('${req.body.firstname}','${req.body.lastname}','${req.body.othername}','${req.body.email}','${req.body.phone_number}','${req.body.username}','${hashedPassword}')returning *;`)
         .then (newUser => {
             return res.status(201).send({
                 "status": 201,
                 "success": "you have successfully created an account",
-                data: newUser.rows
 });
 }).catch( error => {
   console.log(error);
